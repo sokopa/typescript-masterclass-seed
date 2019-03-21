f08abb78a1a38f5d4f6a2245b573fdd43c32244c
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-21 21:18:17 +0200

    keyof

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,14 @@
-console.log(typeof ''); //'string'
-console.log(typeof []); // object
-
 const person = {
   name: 'Todd',
   age: 27,
 };
 
-console.log(typeof person);
-
 type Person = typeof person;
+type PersonKeys = keyof Person;
+
+type PersonTypes = Person[PersonKeys];
 
 const anotherPerson: Person = {
   name: 'John',
   age: 30,
 };
-
-console.log(anotherPerson);
-// JS
-// typeof person // 'object'
