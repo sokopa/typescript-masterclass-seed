84b97bdfbce6fb1944f742464444a016da7d30ce
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-21 21:42:45 +0200

    partial mapped type

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,17 @@ interface Person {
   age: number;
 }
 
-// interface ReadonlyPerson{
-//   readonly name: string;
-//   readonly age : number;
-// }
+// type MyPartial<T> = { [P in keyof T]?: T[P] };
+
+function updatePerson(person: Person, prop: Partial<Person>) {
+  return { ...person, ...prop };
+}
 
 const person: Person = {
   name: 'Todd',
   age: 27,
 };
 
-person.name = 'ABC';
-
-type MyReadonly<T> = { readonly [P in keyof T]: T[P] };
-
-function freeze<T>(obj: T): Readonly<T> {
-  return Object.freeze(obj);
-}
-
-const newPerson = freeze(person);
-// newPerson.age = 1000; // cant touch this
+console.log(person);
+let newPerson = updatePerson(person, { name: 'ABC' });
+console.log(newPerson);
