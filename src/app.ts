94da5f47ef8210eaa8c375deeef6d9f7b4ca57b3
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-21 21:24:13 +0200

    keyof, generics, lookup type

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,13 @@ const person = {
 
 type Person = typeof person;
 type PersonKeys = keyof Person;
-
 type PersonTypes = Person[PersonKeys];
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+const personName = getProperty(person, 'name');
 
 const anotherPerson: Person = {
   name: 'John',
