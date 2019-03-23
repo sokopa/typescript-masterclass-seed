fcb52aa1af5d3ea067e64636695ff469f3168652
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-23 23:20:08 +0200

    function overloads + generics

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,14 @@
-class Pizza {
-  constructor(private name: string, private price: number) {}
-}
-
-// Generics
-class List<T> {
-  private list: T[];
-
-  addItem(item: T): void {
-    this.list.push(item);
+function reverse(str: string): string;
+function reverse<T>(arr: T[]): T[];
+function reverse<T>(stringOrArray: string | T[]): string | T[] {
+  if (typeof stringOrArray === 'string') {
+    return stringOrArray
+      .split('')
+      .reverse()
+      .join('');
   }
-
-  getList(): T[] {
-    return this.list;
-  }
+  return stringOrArray.slice().reverse();
 }
-
-const list = new List<Pizza>();
-// list.addItem('string');
-// list.addItem(5);
-list.addItem(new Pizza('Pepperoni', 15));
-const pizzas = list.getList();
-
-class Coupon {
-  constructor(private name: string) {}
-}
-
-const anotherList = new List<Coupon>();
-
-anotherList.addItem(new Coupon('PIZZA25'));
+console.log(reverse('Pepperoni'));
+console.log(reverse(['bacon', 'pepperoni', 'chili', 'mushrooms']));
+console.log(reverse([1, 2, 3, 4]));
