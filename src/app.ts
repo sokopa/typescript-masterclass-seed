681cbcc9fadac092a2ed1b6d723085e9cdc4b0d5
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-23 11:53:05 +0200

    userDefined typeguard  - start

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,3 @@
-class Foo {
-  bar() {}
-}
-const bar = new Foo();
-// console.log(bar instanceof Foo);
-// console.log(Object.getPrototypeOf(bar) === Foo.prototype);
-
 class Song {
   constructor(public title: string, public duration: number) {}
 }
@@ -12,13 +5,6 @@ class Song {
 class Playlist {
   constructor(public name: string, public songs: Song[]) {}
 }
-
-// function getItemName(item: Song | Playlist) {
-//   if ((item as Song).title) {
-//     return (item as Song).title;
-//   }
-//   return (item as Playlist).name;
-// }
 
 function getItemName(item: Song | Playlist) {
   if (item instanceof Song) return item.title;
