60931863c597c5e33e4790c6e322495f9be725a0
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-24 13:27:17 +0200

    emit declarations from tsc

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,4 @@
-import * as _ from 'lodash';
-
-_.chunk([1, 2, 3, 4], 2);
-
-_.mixin({
-  log(item: string) {
-    console.log(':::', item);
-  },
-});
-
-_.log('Hello!');
+export class Foo {
+  constructor(public name: string) {}
+  bar(age: number) {}
+}
